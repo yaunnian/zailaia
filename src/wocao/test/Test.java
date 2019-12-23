28885c7f330839ef157af5c516c3c2dc242dfa87
yuannian <zyfyuannian@sina.com>
2019-12-23 22:14:26 +0800

    撒旦撒

--- src/wocao/test/Test.java
+++ src/wocao/test/Test.java
@@ -5,5 +5,8 @@ public class Test {
 		System.out.println("难啊");
 		System.out.println("啥啊");
 		System.out.println("就这样");
+		System.out.println("烦死了");
+		System.out.println("wocao");
+		System.out.println("wocao");
 	}
 }
